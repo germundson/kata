42337661e0d9adbaf2d5adb85fb6bedc329eb0cc
Erik Germundson <erik.germundson@buypass.no>
2019-01-04 09:52:04 +0100

    cleanup

--- kata-tdd-common/src/main/java/com/ambita/kata/common/StringToNumbers.java
+++ kata-tdd-common/src/main/java/com/ambita/kata/common/StringToNumbers.java
@@ -2,48 +2,46 @@ package com.ambita.kata.common;
 
 public class StringToNumbers {
 
-  private static final String REGEX_FOR_NUMBER = "^-?[0-9]\\d*(\\.\\d+)?$";
+    private static final String REGEX_FOR_NUMBER = "^-?[0-9]\\d*(\\.\\d+)?$";
 
-  Boolean isEmpty(String numberString) {
-    return numberString == null || numberString.isEmpty();
-  }
-
-  Integer parseNumber(String number) {
-    if (number == null || !number.matches(REGEX_FOR_NUMBER)) {
-      return 0;
+    final Boolean isEmpty(String numberString) {
+        return numberString == null || numberString.isEmpty();
     }
 
-    return Integer.parseInt(number);
-  }
+    final Integer parseNumber(String number) {
+        if (number == null || !number.matches(REGEX_FOR_NUMBER)) {
+            return 0;
+        }
 
-  public int add(String numbers, String delimiter) {
-    if (isEmpty(numbers)) {
-      return 0;
+        return Integer.parseInt(number);
     }
-    int sum = 0;
-    StringBuilder errorContent = new StringBuilder();
-    String tmpNumbers = numbers.replace("\n", delimiter);
-    for (String number : tmpNumbers.split(delimiter)) {
-      int nr = parseNumber(number);
-      sum += nr;
 
-      if (nr < 0) {
+    public final int add(String numbers, String delimiter) {
+        if (isEmpty(numbers)) {
+            return 0;
+        }
+        int sum = 0;
+        StringBuilder errorContent = new StringBuilder();
+        String tmpNumbers = numbers.replace("\n", delimiter);
+        for (String number : tmpNumbers.split(delimiter)) {
+            int nr = parseNumber(number);
+            sum += nr;
+
+            if (nr < 0) {
+                if (errorContent.length() > 0) {
+                    errorContent.append(",");
+                } else if (errorContent.length() < 0) {
+
+                } else {
+
+                }
+                errorContent.append(nr);
+            }
+        }
         if (errorContent.length() > 0) {
-          errorContent.append(",");
+            throw new NumberFormatException("Number(s) " + errorContent.toString() + " is not allowed");
         }
-        else if (errorContent.length() < 0) {
-
-        }
-        else {
-
-        }
-        errorContent.append(nr);
-      }
+        return sum;
     }
-    if (errorContent.length() > 0) {
-      throw new NumberFormatException("Number(s) " + errorContent.toString() + " is not allowed");
-    }
-    return sum;
-  }
 }
 
--- kata-tdd-common/src/test/java/com/ambita/kata/common/StringToNumbersTest.java
+++ kata-tdd-common/src/test/java/com/ambita/kata/common/StringToNumbersTest.java
@@ -1,65 +1,63 @@
 package com.ambita.kata.common;
 
-import java.math.BigInteger;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.math.BigInteger;
 
-public class StringToNumbersTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-  private static final int EXPECTED_RESULT_ZERO = BigInteger.ZERO.intValue();
-  private static final int EXECPTED_RESULT_ONE = BigInteger.ONE.intValue();
-  private static final int EXECPTED_RESULT_MINUS_ONE = BigInteger.ONE.negate().intValue();
+class StringToNumbersTest {
 
-  @DisplayName("Test null input")
-  @Test
-  public void testIsEmpty_nullIn() {
+    private static final int EXPECTED_RESULT_ZERO = BigInteger.ZERO.intValue();
+    private static final int EXECPTED_RESULT_ONE = BigInteger.ONE.intValue();
+    private static final int EXECPTED_RESULT_MINUS_ONE = BigInteger.ONE.negate().intValue();
 
-    assertTrue(new StringToNumbers().isEmpty(null));
-  }
+    @DisplayName("Test null input")
+    @Test
+    void testIsEmpty_nullIn() {
 
-  @DisplayName("Test empty input")
-  @Test
-  public void testIsEmpty_emptyIn() {
+        assertTrue(new StringToNumbers().isEmpty(null));
+    }
 
-    assertTrue(new StringToNumbers().isEmpty(""));
-  }
+    @DisplayName("Test empty input")
+    @Test
+    void testIsEmpty_emptyIn() {
 
-  @DisplayName("Test input with content")
-  @Test
-  public void testIsEmpty_stringIn() {
+        assertTrue(new StringToNumbers().isEmpty(""));
+    }
 
-    assertFalse(new StringToNumbers().isEmpty("-1"));
-  }
+    @DisplayName("Test input with content")
+    @Test
+    void testIsEmpty_stringIn() {
 
-  @DisplayName("Test input that should result in zero")
-  @ParameterizedTest
-  @ValueSource(strings = {  "","D" })
-  public void testParseNumber_ExpectedZero(String value) {
-    assertEquals(Integer.valueOf(EXPECTED_RESULT_ZERO), new StringToNumbers().parseNumber(value));
-  }
+        assertFalse(new StringToNumbers().isEmpty("-1"));
+    }
 
-  @DisplayName("Test input Null that should result in zero")
-  @Test
-  public void testParseNumber_NullInExpectedZero() {
-    assertEquals(Integer.valueOf(EXPECTED_RESULT_ZERO), new StringToNumbers().parseNumber(null));
-  }
+    @DisplayName("Test input that should result in zero")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "D"})
+    void testParseNumber_ExpectedZero(String value) {
+        assertEquals(Integer.valueOf(EXPECTED_RESULT_ZERO), new StringToNumbers().parseNumber(value));
+    }
 
-  @DisplayName("Test input that should result in positive number")
-  @Test
-  public void testParseNumber_positivIn() {
-    assertEquals(Integer.valueOf(EXECPTED_RESULT_ONE), new StringToNumbers().parseNumber("1"));
-  }
+    @DisplayName("Test input Null that should result in zero")
+    @Test
+    void testParseNumber_NullInExpectedZero() {
+        assertEquals(Integer.valueOf(EXPECTED_RESULT_ZERO), new StringToNumbers().parseNumber(null));
+    }
 
-  @DisplayName("Test input that should result in negative number")
-  @Test
-  public void testParseNumber_negativeIn() {
-    assertEquals(Integer.valueOf(EXECPTED_RESULT_MINUS_ONE), new StringToNumbers().parseNumber("-1"));
-  }
+    @DisplayName("Test input that should result in positive number")
+    @Test
+    void testParseNumber_positivIn() {
+        assertEquals(Integer.valueOf(EXECPTED_RESULT_ONE), new StringToNumbers().parseNumber("1"));
+    }
+
+    @DisplayName("Test input that should result in negative number")
+    @Test
+    void testParseNumber_negativeIn() {
+        assertEquals(Integer.valueOf(EXECPTED_RESULT_MINUS_ONE), new StringToNumbers().parseNumber("-1"));
+    }
 }
